a7605e75d24d2cb53136ff2f997735e59fc51aa0
Rhys Ash <rhys.ash@hotmail.com>
2024-04-26 13:38:45 +0100

    started initial build of asgs while fixing the commented TODOs

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# TODO: insert resources here.
 data "azurerm_resource_group" "parent" {
   count = var.location == null ? 1 : 0
 
   name = var.resource_group_name
 }
 
-# TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-resource "azurerm_resource_group" "TODO" {
-  location = coalesce(var.location, local.resource_group_location)
-  name     = var.name # calling code must supply the name
+resource "azurerm_application_security_group" "this" {
+  name                = var.name
+  location            = coalesce(var.location, local.resource_group_location)
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
 }
 
 # required AVM resources interfaces
@@ -17,15 +17,14 @@ resource "azurerm_management_lock" "this" {
 
   lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.name}")
-  scope      = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  scope      = azurerm_application_security_group.this.id
 }
 
 resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignments
 
   principal_id                           = each.value.principal_id
-  scope                                  = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-  condition                              = each.value.condition
+  scope                                  = azurerm_application_security_group.this.id
   condition_version                      = each.value.condition_version
   delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
   role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
