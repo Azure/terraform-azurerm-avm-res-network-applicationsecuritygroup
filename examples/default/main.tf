d155142a77ed01d5a95fffe9161be3df74aecc7c
Bilal Amjad <79409563+mbilalamjad@users.noreply.github.com>
2025-01-13 20:41:57 -0800

    Feature/initial asg (#66)
    * started initial build of asgs while fixing the commented TODOs
    
    * pre-commit docs
    
    * fix broken interfaces from linter and added example for pr check
    
    ---------
    
    Co-authored-by: Rhys Ash <rhys.ash@hotmail.com>
    Co-authored-by: Ciucur Daniel <ciucur.daniel14@gmail.com>

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -16,8 +16,6 @@ provider "azurerm" {
   features {}
 }
 
-
-## Section to provide a random Azure region for the resource group
 # This allows us to randomize the region for the resource group.
 module "regions" {
   source  = "Azure/regions/azurerm"
@@ -29,7 +27,6 @@ resource "random_integer" "region_index" {
   max = length(module.regions.regions) - 1
   min = 0
 }
-## End of section to provide a random Azure region for the resource group
 
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
@@ -44,14 +41,11 @@ resource "azurerm_resource_group" "this" {
 }
 
 # This is the module call
-# Do not specify location here due to the randomization above.
-# Leaving location as `null` will cause the module to use the resource group location
-# with a data source.
-module "test" {
+module "asg" {
   source = "../../"
   # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
   # ...
-  enable_telemetry    = var.enable_telemetry # see variables.tf
-  name                = "TODO"               # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
+  enable_telemetry    = var.enable_telemetry
+  name                = module.naming.application_security_group.name_unique
   resource_group_name = azurerm_resource_group.this.name
 }
